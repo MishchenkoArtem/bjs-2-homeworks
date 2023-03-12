b1db3754be47e82e9c44bb56c314fb8c7bc7c6c3
TheNesh <The.Nesh@yandex.ru>
2023-03-12 19:53:23 +0300

    function-task-2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -10,21 +10,65 @@ function getArrayParams(...rest) {
 }
 
 function summElementsWorker(...rest) {
-
+  if (rest.length === 0) {
+    return 0;
+  } else {
+    return rest.reduce((accumulator, currentValue) => accumulator + currentValue);
+  }
 }
 
 function differenceMaxMinWorker(...rest) {
+  const max = Math.max(...rest);
+  const min = Math.min(...rest);
 
+  if (rest.length === 0) {
+    return 0;
+  } else {
+    return max - min;
+  }
 }
 
 function differenceEvenOddWorker(...rest) {
+  if (rest.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let i = 0; i < rest.length; i++) {
+    if (rest[i] % 2 === 0) {
+      sumEvenElement += rest[i];
+    } else {
+      sumOddElement += rest[i];
+    }
+  }
+
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...rest) {
+  if (rest.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+
+  for (let i = 0; i < rest.length; i++) {
+    if (rest[i] % 2 === 0) {
+      sumEvenElement += rest[i];
+      countEvenElement += 1;
+    }
+  }
+
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
+  const maxWorkerResult = -Infinity;
 
+  for (let i = 0; i < arrOfArr.length; i++) {
+    maxWorkerResult = func(arrOfArr[i]);
+  }
 }
