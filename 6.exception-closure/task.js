235998b0df4644aeb1f51b2355805d74f40951d0
TheNesh <The.Nesh@yandex.ru>
2023-03-29 17:26:42 +0300

    exception-closure/task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(args) {
+    if (isNaN(args)) {
+        throw new Error('Невалидное значение');
+    } else {
+        return Number.parseFloat(args);
+    }
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || b + c < a || c + a < b) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return +s.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() { return "Ошибка! Треугольник не существует"},
+            get perimeter() { return "Ошибка! Треугольник не существует"}
+        }
+    }
+};
